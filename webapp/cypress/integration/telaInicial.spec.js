ab208f4327f1a011e87cc69b6ae7075d445512f6
guiamado <amado.silva133@gmail.com>
2019-06-11 13:23:15 -0300

    [ADD] Colocando teste de cadastro de usuario

--- webapp/cypress/integration/telaInicial.spec.js
+++ webapp/cypress/integration/telaInicial.spec.js
@@ -3,25 +3,26 @@ describe('Testando Tela Inicial', function () {
         telaInicial();
     });
 
-    // it('Cadastrar', function () {
-    //     rota('[href="/cadastrar"]');
-    //
-    //     cy.get('[aria-label="Nome"]').type('Nome Teste');
-    //
-    //     cy.get('[aria-label="E-mail"]').type('email.comunicacao@gmail.com');
-    //
-    //     cy.get('[aria-label="Senha"]').type('123456');
-    //
-    //     cy.get(':nth-child(1) > .v-list__tile > .v-list__tile__content > .v-input > .v-input__control > .v-input__slot > .v-input--selection-controls__input > .v-input--selection-controls__ripple').click();
-    //
-    //     cy.get('.v-btn').click();
-    //
-    //     cy.get('.v-snack__content').contains('Cadastro Realizado com Sucesso!');
-    //     cy.logout();
-    // });
+    it('Cadastrar', function () {
+        cy.get('span > .v-btn > .v-btn__content').click();
+        cy.wait(1000);
+
+        cy.get('[aria-label="Nome"]').type('Nome Teste');
+
+        cy.get('[aria-label="CPF"]').type('01234567899');
+        
+        cy.get('[aria-label="E-mail"]').type('email.comunicacao@gmail.com');
+
+        cy.get('[aria-label="Senha"]').type('123456');
+
+        cy.get(':nth-child(1) > .v-list__tile > .v-list__tile__content > .v-input > .v-input__control > .v-input__slot > .v-input--selection-controls__input > .v-input--selection-controls__ripple').click();
+
+        cy.get('.v-btn').click();
+    });
 
     it('Recuperar Senha', function () {
         cy.get('.caption > .v-btn__content').click();
+        cy.wait(1000);
         
         cy.url().should('eq', Cypress.env('VUE_APP_URL') + 'recuperar');
 
