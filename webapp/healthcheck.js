14dbd18c3a41f6f2c8d0fc7a38f33149ebf2c307
vinnyfs89 <viniciusfesil@gmail.com>
2019-06-19 20:08:23 -0300

    [FIX] Ajustes para healthcheck do webapp

--- webapp/healthcheck.js
+++ webapp/healthcheck.js
@@ -1,25 +1,30 @@
 const http = require('http');
 
 const options = {
-    host: process.env.VUE_WEBAPP_HOST,
-    port: process.env.VUE_WEBAPP_PORT,
-    timeout: 2000,
+    // host: process.env.VUE_WEBAPP_HOST,
+    // port: process.env.VUE_WEBAPP_PORT,
+    host: 'localhost',
+    port: '8080',
+    timeout: 1000,
 };
 
-console.log(process.env.VUE_WEBAPP_HOST, process.env.VUE_WEBAPP_PORT);
+// process.log(process.env.VUE_WEBAPP_HOST, process.env.VUE_WEBAPP_PORT);
 
 const healthCheck = http.request(options, (res) => {
-    console.log(`HEALTHCHECK STATUS: ${res.statusCode}`);
+    // console.log('12312312312312');
+    // console.log(`HEALTHCHECK STATUS: ${res.statusCode}`);
     if (res.statusCode === 200) {
         process.exit(0);
     } else {
         process.exit(1);
     }
 });
-
+// console.log(healthCheck);
 healthCheck.on('error', (errorListener) => {
-    console.error(['ERROR', errorListener]);
+    // console.error(['ERROR', errorListener]);
     process.exit(1);
 });
 
 healthCheck.end();
+
+export default healthCheck;
